a52d728b013220714accadc1d68d78f8cec1f017
A---- <alan71@free.fr>
2020-03-07 12:35:28 +0100

    Rework of providers

--- shortcodes/MapLeafletShortcode.php
+++ shortcodes/MapLeafletShortcode.php
@@ -9,49 +9,45 @@ class MapLeafletShortcode extends Shortcode {
             $params = $sc->getParameters();
             $twig = $this->twig;
             $config = $this->config->get('plugins.map-marker-leaflet');
-            if (isset($params['style'])) {
-                $style = $this->grav['twig']->processString($params['style']);
-            } else $style = '';
-            switch ($config['provider']) {
-                case 'openstreetmap':
-                    $tilestanza = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-                    $attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors';
-                    $maxzoom = 17;
-                    $style = '';
-                    $apikey = '';
-                    break;
-                case 'thunderforest':
-                    $tilestanza = "https://tile.thunderforest.com/{style}/{z}/{x}/{y}{r}.png?apikey={apikey}";
-                    $attribution =  'Maps &copy; <a href="www.thunderforest.com/">Thunderforest</a> Data &copy; <a href="www.opensteetmap.org/copyright">OpenStreetMap</a> contributors';
-                    $maxzoom = 18;
-                    $apikey = $config['apikey'];
-                    $opts = [
-                        'cycle' ,
-                        'transport' ,
-                        'landscape' ,
-                        'outdoors' ,
-                        'transport-dark' ,
-                        'spinal-map' ,
-                        'pioneer' ,
-                        'mobile-atlas' ,
-                        'neighbourhood'
-                    ]; // this should be taken from a yaml config. Hard code for the time being.
-                    if ( ! $style || ! in_array( $style, $opts) ) $style = $config['t-style'];
-                    break;
-                case 'mapbox':
-                    $tilestanza = "https://api.tiles.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={apikey}";
-                    $attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>';
-                    $maxzoom = 24;
-                    $apikey = $config['apikey'];
-                    $opts = [
-                        'mapbox.streets' ,
-                        'mapbox.outdoors' ,
-                        'mapbox.light' ,
-                        'mapbox.dark' ,
-                        'mapbox.satellite'
-                    ];
-                    if ( ! $style || ! in_array( $style, $opts) ) $style = $config['m-style'];
+
+            $provider = [];
+            if (isset($config['providers'][$config['provider']])) {
+
+                $providerFromList = $config['providers'][$config['provider']];
+                if (!is_array($providerFromList['variants'])) {
+                    $variant = '';
+                }
+                else {
+                    if (isset($params['variant'])) {
+                        $variant = $this->grav['twig']->processString($params['variant']);
+                    // style is deprecated
+                    } elseif (isset($params['style'])) {
+                        $variant = $this->grav['twig']->processString($params['style']);
+                    // m-style & t-style are deprecated configuration options
+                    } else {
+                        $variant = $config['variant'] ?? $config['t-style'] ?? $config['m-style'];
+                    }
+
+                    $variant = in_array($variant, $providerFromList['variants']) ? $variant : $providerFromList['default'];
+                }
+
+                $provider = [
+                    'tilestanza' => $providerFromList['tilestanza'],
+                    'attribution' => $providerFromList['attribution'],
+                    'maxzoom' => $providerFromList['maxzoom'],
+                    'apikey' => $providerFromList['maxzoom'],
+                    'variant' => $variant
+                ];
+                
             }
+            else {
+                $provider = [
+                    'tilestanza' => 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    'attribution' => 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                    'maxzoom' => 17
+                ];
+            }
+
             $markercode = '';
             if (is_string($s) ) {
                 // process any twig variables in the markercode
@@ -68,22 +64,20 @@ class MapLeafletShortcode extends Shortcode {
                 $width = isset( $params['width'])? $params['width'] : '100%';
             }
             $output = $twig->processTemplate('partials/mapleaflet.html.twig',
-                [
-                    'tilestanza' => $tilestanza,
-                    'attribution' => $attribution,
-                    'maxzoom' => $maxzoom,
-                    'apikey' => $apikey,
-                    'style' => $style,
-                    'mapname' =>  isset( $params['mapname'] )? $params['mapname'] : 'map',
-                    'lat' => isset( $params['lat'] )? $params['lat'] : '51.505',
-                    'lng' =>  isset( $params['lng'] )? $params['lng'] : '-0.09',
-                    'zoom' => isset( $params['zoom'] )? $params['zoom'] : '13',
-                    'width' => $width,
-                    'height' => $height,
-                    'classes' => isset( $params['classes'])? $params['classes'] : '',
-                    'scale' => array_key_exists('scale', $params)? 'True' : '', # default is FALSE, when scale is not set
-                    'markercode' => $markercode
-                ]);
+                array_merge(
+                    $provider,
+                    [
+                        'mapname' =>  isset( $params['mapname'] )? $params['mapname'] : 'map',
+                        'lat' => isset( $params['lat'] )? $params['lat'] : '51.505',
+                        'lng' =>  isset( $params['lng'] )? $params['lng'] : '-0.09',
+                        'zoom' => isset( $params['zoom'] )? $params['zoom'] : '13',
+                        'width' => $width,
+                        'height' => $height,
+                        'classes' => isset( $params['classes'])? $params['classes'] : '',
+                        'scale' => array_key_exists('scale', $params)? 'True' : '', # default is FALSE, when scale is not set
+                        'markercode' => $markercode
+                    ]
+                ));
             return $output;
         });
     }
